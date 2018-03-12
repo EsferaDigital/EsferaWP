9d0a81ea5a90c57bd1fce093d9b87690ade6fa2b
Gabriel Zavando <gabrielzavando@gmail.com>
2018-03-12 19:43:09 -0300

    Agergamos el footer y sus estilos

--- footer.php
+++ footer.php
@@ -1,15 +1,47 @@
 	</section>
-	<footer class="container footer">
-		<?php
-			$args = array(
-				'theme_location' => 'menu_social',
-				'container' => 'nav',
-				'container_class' => 'item nav'
-			);
-			
-			wp_nav_menu($args);
-		?>
-	</footer>
-	<?php wp_footer(); ?>
+	<footer class="container-footer">
+		<section class="item-footer">
+			<aside class="container-aside-footer">
+				<h4 class="title-aside-footer">Esfera Digital</h4>
+				<p>Somos una Agencia Digital joven y dinámica. Creemos en un futuro digital altamente integrado y cooperativo en la que lo humano es central y la máquina una aliada que libera y conecta. Nuestra propuesta es la entrega de un servicio cuidado, que acerque el desarrollo tecnológico a las personas, comunicando lo que hacemos y las posibilidaes que hoy ofrece la tecnología, en un lenguaje sencillo. Entendemos que cada cliente es único y trabajamos por crear una identidad digital que refleje dicha unicidad.
+				</p>
+			</aside>
+			<aside class="container-aside-footer">
+				<h4 class="title-aside-footer">Síguenos</h4>
+				<section class="social">
+					<a class="social-icon icon-facebook" href="https://www.facebook.com/miesferadigital/" target="_blank">
+					</a>
+					<a class="social-icon icon-youtube" href="https://www.youtube.com/channel/UC8A9wLh4P7l-etbjnDwbcMQ?view_as=subscriber" target="_blank">
+					</a>
+					<a class="social-icon icon-linkedin" href="https://www.linkedin.com/in/gabrielzavando/" target="_blank">
+					</a>
+					<a class="social-icon icon-github" href="https://github.com/EsferaDigital" target="_blank">
+					</a>
+				</section>
+			</aside>
+			<aside class="container-aside-footer">
+				<h4 class="title-aside-footer">Blog</h4>
+				<section class="footer-blog">
+					<ul class="footer-blog-container">
+						<li class="footer-blog-item"><a class="footer-blog-link" href="#">- Entrada 1</a>
+						</li>
+						<li class="footer-blog-item"><a class="footer-blog-link" href="#">- Entrada 2</a>
+						</li>
+						<li class="footer-blog-item"><a class="footer-blog-link" href="#">- Entrada 3</a>
+						</li>
+						<li class="footer-blog-item"><a class="footer-blog-link" href="#">- Entrada 4</a>
+						</li>
+						<li class="footer-blog-item"><a class="footer-blog-link" href="#">- Entrada 5</a>
+						</li>
+					</ul>
+            </section>
+			 </aside>
+			 <aside class="copyright">
+				<p class="icon-copyright copyright-item">Copyright Esfera Digital 2017-2018
+				</p>
+				<p class="copyright-item">Diseñado por Esfera Digital
+				</p>
+    		</aside>
+  		</section>
+</footer>
 </body>
-</html> 
