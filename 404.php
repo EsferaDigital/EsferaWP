2c07dd2b4a261dfb6b68c14e9954f47af03ac55a
Gabriel Zavando <gabrielzavando@gmail.com>
2018-03-26 20:57:33 -0300

    termine la página de error 404

--- 404.php
+++ 404.php
@@ -1,8 +1,19 @@
 <?php
 get_header();
 printf('
-	<h1 class="error">
-		El archivo 404.php es el archivo que toma por defecto wp para mostrar enlaces erroneos o no encontrados.
-	</h1>
+	<section class="error">
+		<div class="buscar">' . get_search_form(false) . '</div>
+		<h1 class="error-title">Error</h1>
+		<p class="error-subtitle">Página no encontrada</p>
+		<div class="error-main">
+			<p>4</p>
+			<img src="http://localhost:8080/esferadigital/wp-content/uploads/2018/03/logo.png"></img>
+			<p>4</p>
+		</div>
+		<div class="error-text">
+			<p>¡Oops! Lo sentimos, esta página no la hemos encontrado</p>
+			<p>Puedes volver al menú principal o ir a la caja de búsqueda.</p>
+		</div>
+	</section>
 ');
 get_footer();
--- page-ofertas.php
+++ page-ofertas.php
@@ -3,5 +3,6 @@
 Template name: Plantilla para Ofertas
  */
 get_header();
+get_template_part('/plantillas/banner-ofertas');
 get_template_part('/plantillas/ofertas');
 get_footer();
