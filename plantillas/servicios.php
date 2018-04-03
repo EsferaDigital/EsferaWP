8c8e7556d00d6d387a67af766aa74d6224e865ee
Gabriel Zavando <gabrielzavando@gmail.com>
2018-04-03 19:12:03 -0300

    terminamos la estructura y maqueta de servicios

---
+++ plantillas/servicios.php
@@ -0,0 +1,74 @@
+<section class="services">
+	<article class="services-first-title">
+		<h1>Los servicios que necesitas para que tu empresa despegue</h1>
+	</article>
+	<div class="services-item">
+		<article class="services-item-box">
+			<div>icono</div>
+			<h3>Diseño web</h3>
+			<p>Diseñamos bla bla bla</p>
+		</article>
+		<article class="services-item-box">
+			<div>icono</div>
+			<h3>Desarrollo web</h3>
+			<p>Desarrollamos bla bla bla</p>
+		</article>
+		<article class="services-item-box">
+			<div>icono</div>
+			<h3>Mantenimiento y actualización</h3>
+			<p>Realizamos mantenimiento periódico bla bla bla</p>
+		</article>
+		<article class="services-item-box">
+			<div>icono</div>
+			<h3>Hosting</h3>
+			<p>Trabajamos con los más veloces y seguros servidores</p>
+		</article>
+	</div>
+</section>
+<section class="services">
+	<h2 class="services-title">Sistema de <span>trabajo</span></h2>
+	<div class="sub-container">
+		<article class="sub-container-box">
+			<i class="iconos">icono</i>
+			<div class="box-text">
+				<h2>01 Asesoría técnica</h2>
+				<p>Reunión presencial u online con cada cliente para levantar información, resolver dudas y realizar los primeros bosquejos de tu sitio</p>
+			</div>
+		</article>
+		<article class="sub-container-box">
+			<i class="iconos">icono</i>
+			<div class="box-text">
+				<h2>02 Presupuesto</h2>
+				<p>Envío de una cotización contemplando plazos de entrega, formas de pago y acuerdo de trabajo</p>
+			</div>
+		</article>
+		<article class="sub-container-box">
+			<i class="iconos">icono</i>
+			<div class="box-text">
+				<h2>03 Envío de contenidos</h2>
+				<p>Una vez aprobado el presupuesto se realiza el anticipo acordado y se envía el contenido necesario para elaborar el diseño y desarrollo (textos, imágenes, videos, etc.)</p>
+			</div>
+		</article>
+		<article class="sub-container-box">
+			<i class="iconos">icono</i>
+			<div class="box-text">
+				<h2>04 Propuesta de diseño</h2>
+				<p>Enviamos un mockup o prototipo para su revisión y/o aprobación. Podrá realizar este paso varias veces (según lo acordado anteriormente) antes de su aprobación definitiva</p>
+			</div>
+		</article>
+		<article class="sub-container-box">
+			<i class="iconos">icono</i>
+			<div class="box-text">
+				<h2>05 Desarrollo</h2>
+				<p>Una vez aprobado el mockup o prototipo se comienza a pasar a código para subirlos a los servidores contratados y dejarlo 100% online</p>
+			</div>
+		</article>
+		<article class="sub-container-box">
+			<i class="iconos">icono</i>
+			<div class="box-text">
+				<h2>06 Entrega Final</h2>
+				<p>Se entrega una carpeta digital con todos los archivos en los formatos correspondientes a cada proyecto,en modo desarrollo (legile por cualquier otro programador), se entrega acceso a la administración del sitio según corresponda y se publica el sitio web en servidor. Al mismo tiempo, se ejecuta el pago final del proyecto</p>
+			</div>
+		</article>
+	</div>
+</section>
