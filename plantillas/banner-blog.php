33bad16226b615506f1acc347d6977cf81d8156d
Gabriel Zavando <gabrielzavando@gmail.com>
2018-04-09 23:08:57 -0300

    Mejore todos los banner

--- plantillas/banner-blog.php
+++ plantillas/banner-blog.php
@@ -1,9 +1,7 @@
-<div class="banner">
-  <picture class="banner-img">
-    <source srcset="http://localhost:8080/esferadigital/wp-content/uploads/2018/03/blog-banner.jpg" media="(min-width: 768px)" />
-    <img src="http://localhost:8080/esferadigital/wp-content/uploads/2018/03/banner-mobile-blog.jpg" />
-  </picture>
-  <div class="banner-copy-blog">
-    <p class="banner-copy-subtitle-blog">Esfera Digital/<span>Blog</span></p>
-  </div>
+<div class="banner-blog">
+	<div class="banner-title">
+		<p>
+			Esfera Digital/<span>Blog</span>
+		</p>
+	</div>
 </div>
