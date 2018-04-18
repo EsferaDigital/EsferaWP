f53bbbd3ff3c81e3e9d070577226ec110f1ee56e
Gabriel Zavando <gabrielzavando@gmail.com>
2018-04-18 16:33:10 -0300

    Cambié las rutas del hedaer y del banner

--- plantillas/banner.php
+++ plantillas/banner.php
@@ -1,5 +1,5 @@
 <div class="banner">
-  <img class="banner-logo" src="http://localhost:8080/esferadigital/wp-content/uploads/2018/03/logo.png" />
+  <img class="banner-logo" src="https://esferadigital.cl/wp-content/uploads/2018/03/logo.png" />
   <h2 class="banner-subtitle">Sitios web personalizados, adaptables y autoadministrables
   </h2>
   <a class="btn cta">Escríbenos</a>
