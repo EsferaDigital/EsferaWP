02667f6dde208120eed620ddd40de9f549b23bc4
Gabriel Zavando <gabrielzavando@gmail.com>
2018-05-06 18:39:45 -0300

    termine la pagina servicios en tamaño movil

--- plantillas/destacados.php
+++ plantillas/destacados.php
@@ -3,31 +3,31 @@
   <article class="c-destacados-item">
     <aside class="aside-inicio">
       <span class="icon-ampolleta"></span>
-      <h3>Creatividad</h3>
+      <h2>Creatividad</h2>
       <p>Tu presencia en el Mundo está marcada por la imagen que representas, por la identidad que construyes. Nadie en el mundo tiene presencia si no es capaz de crear una identidad que lo distinga.</p>
     </aside>
   </article>
   <article class="c-destacados-item">
     <aside class="aside-inicio">
       <span class="icon-smile"></span>
-      <h3>Amigable</h3>
+      <h2>Amigable</h2>
       <p>Una interfaz clara, sencilla, que explique amigablemente lo que ofreces, que permita interactuar fácil y cómodamente con tu sitio, pensada desde el usuario, también es clave para el éxito de tu presencia en la Web.</p>
     </aside>
   </article>
   <article class="c-destacados-item adaptable">
     <aside class="aside-inicio">
       <span class="icon-adaptativo"></span>
-      <h3>Adaptable</h3>
+      <h2>Adaptable</h2>
       <p>Más del 51% de la navegación se realiza desde dispositivos móviles. Necesitamos contar con un diseño y una programación escalable y adaptable a los nuevos dispositivos y nuevas tecnologías.</p>
     </aside>
   </article>
 </section>
-<section class="c-destacados">
-  <h3 class="c-destacados-title"><span>Servicios</span></h3>
+<section class="c-destacados fondo-destacados">
+  <h3 class="c-destacados-title">Servicios</h3>
   <article class="c-destacados-item destacados-servicios">
     <aside class="aside-inicio">
       <span class="icon-ruler"></span>
-      <h3>Diseño Web</h3>
+      <h2>Diseño Web</h2>
       <p>Diseñamos sitios web a medida.
       </p>
       <p>Analizamos tus necesidades y objetivos para diseñar un sitio web coherente con esas necesidades (un Blog, una web informativa, una e-commerce, etc.).
@@ -41,7 +41,7 @@
   <article class="c-destacados-item destacados-servicios">
     <aside class="aside-inicio">
       <span class="icon-code"></span>
-      <h3>Desarrollo Web</h3>
+      <h2>Desarrollo Web</h2>
       <p>Un sitio web moderno no sólo debe tener una estética cuidada, sino también debe ser desarrollada con un código limpio y ligero
       </p>
       <p>Desarrollamos en base a los estándares recomendados, lo que facilita el posicionamiento en buscadores, con etiquetas semánticas, sin pesadas librerías, con un código optimizado y actualizado</p>
@@ -50,7 +50,7 @@
   <article class="c-destacados-item destacados-servicios">
     <aside class="aside-inicio">
       <span class="icon-refresh"></span>
-      <h3>Actualización</h3>
+      <h2>Actualización</h2>
       <p>Si ya tienes un sitio web pero éste no está adaptado a dispositivos móviles, carga muy lento y/o tiene un diseño viejo, lo más probable es que tengas una alta tasa de rebote</p>
       <p>Refrescamos y modernizamos tu sitio para que se mantenga vigente y asi evites perder las valiosas visitas que recibes</p>
       <p>Actualizamos periódicamente los sitios de nuestros clientes para mejorar aquellos aspectos que requieren cambios estéticos o actualizaciones tecnológicas</p>
@@ -59,7 +59,7 @@
   <article class="c-destacados-item destacados-servicios">
     <aside class="aside-inicio">
       <span class="icon-server"></span>
-      <h3>Hosting</h3>
+      <h2>Hosting</h2>
       <p>Que tu sitio esté permanentemente disponible y responda rápidamente a las visitas que recibe, depende también del hosting en el cual esté alojado</p>
       <p>Trabajamos con servidores veloces y modernos que garantizan estabilidad, seguridad y escalabilidad, de manera tal que las caracterizticas del servidor se adpaten al crecimiento de vuestro sitio</p>
     </aside>
