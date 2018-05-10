1400703f5b540028533fdc006f83fca8ff10eb10
Gabriel Zavando <gabrielzavando@gmail.com>
2018-05-10 12:43:14 -0300

    termine la pagina de ofertas y con ella todo el sitio

--- plantillas/ofertas.php
+++ plantillas/ofertas.php
@@ -1,27 +1,27 @@
 <section class="ofertas">
-	<div class="ofertas-container">
-		<article class="ofertas-item">
-			<h2 class="ofertas-title">Plan Básico</h2>
-			<div class="ofertas-price">$200.000<span>+ IVA</span></div>
-			<ul class="ofertas-detail">
+	<div class="c-ofertas">
+		<article class="c-ofertas-item">
+			<h2 class="c-ofertas-item-title">Plan Básico</h2>
+			<div class="c-ofertas-item-price">$200.000/<span>+ IVA</span></div>
+			<ul class="c-ofertas-item-detail">
 				<li>Diseño Responsive*</li>
 				<li>Dominio + Hosting**</li>
 				<li>3 Páginas web estáticas</li>
 				<li>2 Correos Corporativos</li>
-				<li>Integración con RRSS</li>
+				<li>Integrado con RRSS</li>
 				<li>Formulario de Contacto</li>
-				<li>Tiempo estimado: 2 a 3 semanas</li>
+				<li>Entrega: 2 a 3 semanas</li>
 			</ul>
-			<a href="#" class="btn">solicitar plan</a>
-			<footer class="ofertas-footer">
-				<p>*Plantillas y código pre-desarrollado</p>
-				<p>**Por un Año</p>
+			<a href="https://esferadigital.cl/contacto" class="boton1 boton-ofertas">solicitar plan</a>
+			<footer class="c-ofertas-item-footer">
+				<p>* Plantillas y código pre-desarrollado</p>
+				<p>** Por un Año</p>
 			</footer>			
 		</article>
-		<article class="ofertas-item">
-			<h2 class="ofertas-title">Plan Pyme</h2>
-			<div class="ofertas-price">$360.000<span>+ IVA</span></div>
-			<ul class="ofertas-detail">
+		<article class="c-ofertas-item">
+			<h2 class="c-ofertas-item-title">Plan Pyme</h2>
+			<div class="c-ofertas-item-price">$360.000/<span>+ IVA</span></div>
+			<ul class="c-ofertas-item-detail">
 				<li>Diseño Responsive*</li>
 				<li>Código Personalizado*</li>
 				<li>Dominio + Hosting**</li>
@@ -29,36 +29,36 @@
 				<li>5 Correos Corporativos</li>
 				<li>Formulario de Contacto</li>
 				<li>Integrado con RRSS</li>
-				<li>Integrado con Google Analytics</li>
+				<li>Google Analytics</li>
 				<li>Auto-administración del sitio</li>
-				<li>Tiempo estimado: 4 a 6 semanas</li>
+				<li>Entrega: 4 a 6 semanas</li>
 			</ul>
-			<a href="#" class="btn">solicitar plan</a>
-			<footer class="ofertas-footer">
-				<p>*Basada en plantilla psd ya diseñada. Código desde cero</p>
-				<p>**Por un año</p>
-				<p>***Una de ellas puede ser un Blog</p>
+			<a href="https://esferadigital.cl/contacto" class="boton1 boton-ofertas">solicitar plan</a>
+			<footer class="c-ofertas-item-footer">
+				<p>* Basada en plantilla psd ya diseñada. Código desde cero</p>
+				<p>** Por un año</p>
+				<p>*** Una de ellas puede ser un Blog</p>
 			</footer>			
 		</article>
-		<article class="ofertas-item">
-			<h2 class="ofertas-title">Plan Corporativo</h2>
-			<div class="ofertas-price">$800.000<span>+ IVA</span></div>
-			<ul class="ofertas-detail">
+		<article class="c-ofertas-item">
+			<h2 class="c-ofertas-item-title">Plan Corporativo</h2>
+			<div class="c-ofertas-item-price">$800.000/<span>+ IVA</span></div>
+			<ul class="c-ofertas-item-detail">
 				<li>Diseño y código personalizado</li>
 				<li>Dominio + Hosting *</li>
 				<li>7 Páginas web</li>
 				<li>10 Correos Corporativos</li>
 				<li>Formulario de Contacto</li>
 				<li>Integrado con RRSS</li>
-				<li>Integrado con Google Analytics</li>
+				<li>Google Analytics</li>
 				<li>Dirección en Google Maps</li>
-				<li>Integrado con Chat de vuestra Fan Page</li>
+				<li>Chat de Messenger</li>
 				<li>Auto-administración del sitio</li>
-				<li>Tiempo estimado: A convenir</li>
+				<li>Entrega: A convenir</li>
 			</ul>
-			<a href="#" class="btn">solicitar plan</a>
-			<footer class="ofertas-footer">
-				<p>*Por un año</p>
+			<a href="https://esferadigital.cl/contacto" class="boton1 boton-ofertas">solicitar plan</a>
+			<footer class="c-ofertas-item-footer">
+				<p>* Por un año</p>
 			</footer>
 		</article>
 	</div>
